57189596a69dec125ee89884b370b0a215955a8e
sri <sreelakshmi.ravipati88@gmail.com>
2026-01-13 14:46:02 +0000

    adding post and get request practiced code

---
+++ src/test/java/BasicRequestPractice.java
@@ -0,0 +1,46 @@
+
+import DataFiles.Payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class BasicRequestPractice {
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+// validate if Add Place API is working as expected
+
+
+        //given - all input details
+        //when - Submit the API -[resource,http method]
+        //Then - validate the response
+        RestAssured.baseURI= "https://rahulshettyacademy.com";
+
+        String response=given().queryParam("key", "qaclick123").header("Content-Type","application/json")
+                .body(Payload.addPlace())
+                .when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200)
+                .body("scope",equalTo("APP")).header("Content-Type","application/json;charset=UTF-8")
+                .extract().response().asString();
+
+        //Add place -->update place with new address-->get the place to validate if new address is present or not
+        System.out.println("THE RESPONSE AFTRER ADDING THE PLACE IS"+response);
+
+        JsonPath js=new JsonPath(response);
+        String placeID=js.getString("place_id");
+
+        System.out.println("THE GENERATED placeId is..."+placeID);
+
+        //get place
+
+        given().log().all().queryParam("key","qaclick123")
+                .queryParam("place_id",placeID)
+                .when().get("maps/api/place/get/json")
+                .then().log().all().assertThat().statusCode(200);
+
+    }
+
+}
