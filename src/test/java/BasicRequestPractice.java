a0d0b21b9e8fc6496e3651755eeab0026f803956
sri <sreelakshmi.ravipati88@gmail.com>
2026-01-14 16:26:08 +0000

    adding end to end process of adding,updating,getting,deleting request

--- src/test/java/BasicRequestPractice.java
+++ src/test/java/BasicRequestPractice.java
@@ -2,6 +2,7 @@
 import DataFiles.Payload;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -34,12 +35,50 @@ public class BasicRequestPractice {
 
         System.out.println("THE GENERATED placeId is..."+placeID);
 
-        //get place
+        //get place before updating of the address
 
         given().log().all().queryParam("key","qaclick123")
                 .queryParam("place_id",placeID)
                 .when().get("maps/api/place/get/json")
                 .then().log().all().assertThat().statusCode(200);
+
+        //update place
+        String newAddress="70 Summer walk, USA";
+
+        given().log().all().queryParam("key","qaclick123")
+                .body("{\n" +
+                        "\"place_id\":\""+placeID+"\",\n" +
+                        "\"address\":\""+newAddress+"\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}")
+                .when().put("maps/api/place/update/json")
+                .then().log().all().assertThat().statusCode(200);
+                //.body("msg",equalTo("Address successfully updated"));
+
+
+
+
+        //getting place after updating of the address
+       String  gettingUpdatedResponse=given().log().all().queryParam("key","qaclick123")
+                .queryParam("place_id",placeID)
+                .when().get("maps/api/place/get/json")
+                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+       JsonPath jp2=new JsonPath(gettingUpdatedResponse);
+       String expectedUpdateAddress=jp2.getString("address");
+
+        Assert.assertEquals(newAddress,expectedUpdateAddress);
+
+        //deleting the place
+
+        given().queryParam("key","qaclick123")
+                .body("{\n" +
+                        "    \"place_id\": \""+placeID+"\"\n" +
+                        "}")
+                .when().delete("maps/api/place/delete/json")
+                .then().assertThat().statusCode(200);
+
+
 
     }
 
