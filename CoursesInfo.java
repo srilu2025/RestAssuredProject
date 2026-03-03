62b75197aa2fc2daa98ccbc5ed67802657343da3
sreelakshmi <sreelakshmi.ravipati88@gmail.com>
2026-03-03 11:14:50 +0000

    Add files via upload

---
+++ CoursesInfo.java
@@ -0,0 +1,36 @@
+package PojoPractice;
+
+import java.util.List;
+
+public class CoursesInfo {
+
+    private List<Course> webAutomation;
+    private List<Course> api;
+    private List<Course> mobile;
+
+
+    public List<Course> getWebAutomation() {
+        return webAutomation;
+    }
+
+    public void setWebAutomation(List<Course> webAutomation) {
+        this.webAutomation = webAutomation;
+    }
+
+    public List<Course> getApi() {
+        return api;
+    }
+
+    public void setApi(List<Course> api) {
+        this.api = api;
+    }
+
+    public List<Course> getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(List<Course> mobile) {
+        this.mobile = mobile;
+    }
+
+}
